31848d5d1ab9441d7d44cfe67b2b4b9ba2695c7e
Abirami <meeraabirami89@gmail.com>
2024-02-28 23:52:41 +0530

    commiting login.java by abirami

--- orangehrmproject/src/HRMpack/login.java
+++ orangehrmproject/src/HRMpack/login.java
@@ -13,7 +13,7 @@ public class login {
   public void Launchurl() throws Exception
   {
 	  driver.get("http://localhost/orangehrm/orangehrm-5.6/orangehrm-5.6/web/index.php/auth/login");
-	  System.out.println("url launched");
+	  System.out.println("the website launched sucessfully");
   }
   public void login() throws Exception
   {
@@ -21,6 +21,7 @@ public class login {
 	  driver.findElement(By.name("username")).sendKeys("abirami");
 		driver.findElement(By.name("password")).sendKeys("Abijagan@89");
 		driver.findElement(By.xpath("//html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).click();
+		
 		System.out.println("the login page launched sucessfully");
   }
 }
